27f22374794582e50b8d711368c48a1877aa547d
Kereltje <13039713+Kereltje@users.noreply.github.com>
2023-08-24 02:34:29 +0200

    test multiple instances of xbmcaddon.Addon()

--- service.test.service_settings/service.py
+++ service.test.service_settings/service.py
@@ -1,33 +1,26 @@
-
-import threading
 import xbmc
 import xbmcaddon
 
-addon = xbmcaddon.Addon()
+addon1 = xbmcaddon.Addon()
+addon2 = xbmcaddon.Addon()
+addon3 = xbmcaddon.Addon()
 
-def print_settings(prefix='Thread -'):
+
+def print_settings():
     current_addon = xbmcaddon.Addon()
-    xbmc.log("{} Setting 1 = {}".format(prefix, addon.getSetting('Setting1')), xbmc.LOGWARNING)
-    xbmc.log("{} Setting 2 = {}".format(prefix, addon.getSetting('Setting2')), xbmc.LOGWARNING)
-    xbmc.log("{} Setting 1 current addon = {}".format(prefix, current_addon.getSetting('Setting1')), xbmc.LOGWARNING)
-    xbmc.log("{} Setting 2 current_addon = {}".format(prefix, current_addon.getSetting('Setting2')), xbmc.LOGWARNING)
+    xbmc.log("Setting addon1 = {}".format(addon1.getSetting('Setting1')), xbmc.LOGWARNING)
+    xbmc.log("Setting addon2 = {}".format(addon2.getSetting('Setting1')), xbmc.LOGWARNING)
+    xbmc.log("Setting addon3 = {}".format(addon3.getSetting('Setting1')), xbmc.LOGWARNING)
+    xbmc.log("Setting new addon = {}".format(current_addon.getSetting('Setting1')), xbmc.LOGWARNING)
 
 
 def run():
     monitor = xbmc.Monitor()
-    state = False
     while not monitor.abortRequested():
         print_settings()
-        state = not state
-        addon.setSetting('Setting2', str(state).lower())
         monitor.waitForAbort(10)
 
 
 if __name__ == '__main__':
-    # run()
-    threading.Thread(target=run, daemon=True).start()
-    monitor = xbmc.Monitor()
-    monitor.waitForAbort(5)
-    while not monitor.abortRequested():
-        print_settings('Main -')
-        monitor.waitForAbort(10)
+    run()
+
